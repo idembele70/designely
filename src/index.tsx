d21e6f1a2a8ab0985fda650b35f83ed3ba20521d
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2023-03-14 22:40:15 +0100

    [App] deployed to GH-pages

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/designely">
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyle />
         <App />
